ef5d3ec3c845e805f52b224121517e098ed16bb8
Mayankjoshi341 <mayankjoshi0341@gmail.com>
2026-03-02 23:21:40 +0530

    fix: update parameter name in load_detail_data function for consistency

--- MediScan/src/data/load_data.py
+++ MediScan/src/data/load_data.py
@@ -3,9 +3,9 @@ def load_data(DATA_DIR):
     print("data loaded successfully.")
     main =  pd.read_csv(DATA_DIR / "dataset.csv")
     return main
-def load_detail_data(DATA_DIR):
-                description =  pd.read_csv(DATA_DIR / "symptom_Description.csv")
-                precaution =  pd.read_csv(DATA_DIR / "symptom_precaution.csv")
-                homecare =  pd.read_csv(DATA_DIR / "disease_homecare_data.csv")
+def load_detail_data(RAW_DATA_DIR):
+                description =  pd.read_csv(RAW_DATA_DIR / "symptom_Description.csv")
+                precaution =  pd.read_csv(RAW_DATA_DIR / "symptom_precaution.csv")
+                homecare =  pd.read_csv(RAW_DATA_DIR / "disease_homecare_data.csv")
                 return description , precaution , homecare
         
